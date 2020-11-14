49a932a3f993d4dd03ac374e7e599b1dc5f1307f
ItsJustCory <1910307@swanse.ac.uk>
2020-11-14 19:11:39 +0000

    Created PlayerProfile and Player Classes
    Myself and steffan did as much as possible with the current classes in the system in regards to these classes. (Commented these regions in the code *Mostly tiles*)
    
    Co-Authored-By: steffan01 <73750117+steffan01@users.noreply.github.com>

---
+++ Labyrinth/src/Player.java
@@ -0,0 +1,95 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.scene.image.Image;
+
+public class Player
+{
+	// private FloorTile location; "Remove once floor tile class is done."
+	private String imgPath;
+	private PlayerProfile playerProfile;
+	private Player[] prevBtPlayers = new Player[4];
+	// private ArrayList<ActionTile> hand = new ArrayList<ActionTile>(); "Remove once action tile class is done."
+	// insert backTiles here
+	private int movesPerTurn;
+	
+	public Player(//FloorTile location, "Remove once floor tile class is done."
+			String imgPath, PlayerProfile playerProfile)
+	{
+		this.imgPath = imgPath;
+		this.playerProfile = playerProfile;
+	}
+
+	public PlayerProfile getPlayerProfile() {
+		return playerProfile;
+	}
+
+	public Player[] getPrevBtPlayers()
+	{
+		return prevBtPlayers;
+	}
+	
+	public void addPrevBtPlayers(Player btdPlayer)
+	{
+		for (int i = 0; i < prevBtPlayers.length-1; i++)
+		{
+			if (prevBtPlayers[i] == null)
+			{
+				prevBtPlayers[i] = btdPlayer;
+				break;
+			}
+		}
+	}
+	
+	/*
+	 * uncomment when action tile class is done
+	 * 
+	 * public ArrayList<ActionTile> getHand()
+	 * {
+	 *     return hand;
+	 * }
+	 */
+	
+	public Image getImage()
+	{
+		try
+		{
+			FileInputStream inputStream = new FileInputStream(imgPath);
+			Image image = new Image(inputStream);
+			return image;
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("Can not find the image file.");
+			System.exit(0);
+		}
+		return null;
+	}
+	
+	public int getMovesPerTurn()
+	{
+		return movesPerTurn;
+	}
+	
+	public void setMovesPerTurn(int num)
+	{
+		if (num > 2)
+		{
+			num = 2;
+		}
+		else if (num < 0)
+		{
+			num = 0;
+		}
+		
+		movesPerTurn = num;
+	}
+	
+	/*
+	 * uncomment when floor tile class is done
+	 * 
+	 * private void updateGetBackTiles(FloorTile newTile)
+	 * {
+	 *	backTiles.add(newTile);
+	 * }
+	*/
+}
---
+++ Labyrinth/src/PlayerProfile.java
@@ -0,0 +1,48 @@
+
+public class PlayerProfile
+{
+	private int losses;
+	private int wins;
+	private String name;
+	private int gamesPlayed;
+	
+	public PlayerProfile(int losses, int wins, String name, int gamesPlayed)
+	{
+		this.losses = losses;
+		this.wins = wins;
+		this.name = name;
+		this.gamesPlayed = gamesPlayed;
+	}
+
+	public int getLosses()
+	{
+		return losses;
+	}
+
+	public void incLosses()
+	{
+		losses ++;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+	
+	public void incWins()
+	{
+		wins ++;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getGamesPlayed() {
+		return gamesPlayed;
+	}
+
+	public void incGamesPlayed()
+	{
+		gamesPlayed ++;
+	}
+}
