7d37ec7828a72653243c7f8fb6f1a1bce8f70465
steffan01 <73750117+steffan01@users.noreply.github.com>
2020-12-06 17:54:44 +0000

    Java Docs

--- Labyrinth/src/Player.java
+++ Labyrinth/src/Player.java
@@ -4,6 +4,11 @@ import javafx.scene.image.Image;
 
 import java.util.ArrayList;
 
+/**
+* This Class represents the Player.
+* @author Cory Lagdon and Steffan Long
+* @version 1.0
+*/
 public class Player
 {
 	// private FloorTile location; "Remove once floor tile class is done."
@@ -16,7 +21,13 @@ public class Player
 	private int movesPerTurn = 1;
 	private FloorTile playerLocation;
 	
-	
+	/**
+	 * Creates a Player for the board.
+	 * @param imgPath The path of image of the player.
+	 * @param playerProfile The object playerProfile.
+	 * @param location The location of the player on the board.
+	 * @param playerImage The image of the player.
+	 */
 	public Player(String imgPath, PlayerProfile playerProfile, FloorTile location, Image playerImage) {
 		this.imgPath = imgPath;
 		this.playerProfile = playerProfile;
@@ -26,19 +37,35 @@ public class Player
 		updateGetBackTiles(location);
 	}
 
+	/**
+	 * Get the playerImage.
+	 * @return The player image.
+	 */
 	public Image getPlayerImage(){
 		return playerImage;
 	}
 
+	/**
+	 * Get the playerProfile.
+	 * @return The player profile.
+	 */
 	public PlayerProfile getPlayerProfile() {
 		return playerProfile;
 	}
 
+	/**
+	 * Get the list of previous players.
+	 * @return The prevBtPlayers.
+	 */
 	public Player[] getPrevBtPlayers()
 	{
 		return prevBtPlayers;
 	}
 	
+	/**
+	 * Adds a player to the list of backtracked players.
+	 * @param btdPlayer The player who has been backtracked.
+	 */
 	public void addPrevBtPlayers(Player btdPlayer)
 	{
 		for (int i = 0; i < prevBtPlayers.length-1; i++)
@@ -51,11 +78,20 @@ public class Player
 		}
 	}
 
+	/**
+	 * Gets the hand of the player.
+	 * @param index The position of the tile in the array.
+	 * @return ActionTile An action tile from the array.
+	 */
 	public ActionTile getHand(int index)
 	{
 	    return hand.get(index);
 	}
 
+	/**
+	 * Adds new action tile to the players hand.
+	 * @param newTile The tile to be added to the player's hand.
+	 */
 	public ArrayList<ActionTile> getWholeHand()
 	{
 		return hand;
@@ -65,23 +101,44 @@ public class Player
 		hand.add(newTile);
 	}
 
+	/**
+	 * Get the size of the players hand.
+	 * @return hand.size() The size of the player's hand.
+	 */
 	public int sizeOfHand(){
 		return hand.size();
 	}
+
+	/**
+	 * Remove a tile from the players hand.
+	 * @param tile The tile that needs to get removed from the player's hand.
+	 */
 	public void removeHandTile(ActionTile tile) {
 		hand.remove(tile);
 	}
 
+	/**
+	 * Gets the image path of the player image.
+	 * @return imgPath The path of the player's image.
+	 */
 	public String getImage()
 	{
 		return imgPath;
 	}
 	
+	/**
+	 * Gets the number of moves the players can do in a turn.
+	 * @return movesPerTurn The number of moves the player can do in a turn.
+	 */
 	public int getMovesPerTurn()
 	{
 		return movesPerTurn;
 	}
 	
+	/**
+	 * Sets the amount of moves the player can do this turn.
+	 * @param num The amount of moves the player can do in a turn. 
+	 */
 	public void setMovesPerTurn(int num)
 	{
 		if (num > 2)
